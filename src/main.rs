9de9239db24b82c961e1caf3951949ebbe2a0b26
Adrien Allard <aallard@amplitude-studios.com>
2021-12-04 16:04:23 +0100

    Day 4

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,112 @@ use std::path::Path;
 use regex::Regex;
 
 fn main() {
-    day3s2();
+    day4();
     return;
+    day3s2();
     day3s1();
     day2s2();
     day2s1();
     day1s2();
     day1s1();
+}
+
+// DAY 4
+
+fn day4() {
+    let (numbers, mut grids) = {
+        let regex = Regex::new(r"^\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*$").unwrap();
+        let mut numbers: Vec<u8> = Vec::new();
+        let mut grids: Vec<Grid> = Vec::new();
+        
+        if let Ok(lines) = read_lines("day4.txt") {
+            let mut grid: Grid = Grid::new();
+            let mut row = 0;
+            for line in lines {
+                if let Ok(ip) = line {
+                    if numbers.len() == 0 {
+                        numbers = ip.split(',').map(|str| str.parse::<u8>().unwrap()).collect();  
+                    } else {
+                        if let Option::Some(capture) = regex.captures(&ip) {
+                            let mut col = 0;
+                            for i in 1..capture.len() {
+                                grid.grid[row][col].number = capture.get(i).unwrap().as_str().parse::<u8>().unwrap();
+                                col += 1;
+                            }
+
+                            row += 1;
+                            if row == 5 {
+                                // New grid
+                                grids.push(grid);
+                                grid = Grid::new();
+                                row = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        (numbers, grids)
+    };
+
+    let mut first_to_win = -1;
+    let mut last_to_win = -1;
+    for n in numbers {
+        for i in (0..grids.len()).rev() {
+            let grid = &mut grids[i];
+            grid.mark(n);
+            let (victory, unmarked_sum) = grid.check_victory();
+            if victory {
+                if first_to_win < 0 { first_to_win = n as i32 * unmarked_sum };
+                last_to_win = n as i32 * unmarked_sum;
+                grids.remove(i);
+            }
+        }
+    }
+
+    println!("Day4.1: First grid to win: {}", first_to_win);
+    println!("Day4.2: Last grid to win: {}", last_to_win);
+}
+
+struct Grid {
+    grid: [[ Cell; 5]; 5],
+}
+
+impl Grid {
+    fn new() -> Self {
+        Self {
+          grid: [[Cell {number: 0, marked: false}; 5]; 5]
+        }
+    }
+
+    fn mark(&mut self, n: u8) {
+        self.grid.iter_mut().for_each(|line| line.iter_mut().for_each(|cell| if cell.number == n { cell.marked = true }));
+    }
+
+    fn check_victory(&self) -> (bool, i32) {
+        let mut unmarked_sum = 0i32;
+        let mut col_count = [0i32; 5];
+        let mut win = false;
+        for (_, line) in self.grid.iter().enumerate() {
+            let mut row_count = 0;
+            for (col, cell) in line.iter().enumerate() {
+                if cell.marked { row_count += 1; col_count[col] += 1 } else { unmarked_sum += cell.number as i32 };
+            }
+
+            if row_count == 5 { win = true };
+        }
+
+        col_count.iter().for_each(|c| if *c == 5 { win = true });
+
+        (win, unmarked_sum)
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Cell {
+    number: u8,
+    marked: bool,
 }
 
 // DAY 3
